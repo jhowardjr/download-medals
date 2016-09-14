57dcd9ecf20446423fede5dfad85d97ed39d5376
Johnathan Howard <johnathan.howardjr@gmail.com>
2016-09-14 19:10:59 -0400

    initial commit

---
+++ download-medals.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import urllib2
+import sqlite3
+import urllib
+from bs4 import BeautifulSoup
+
+wiki = urllib2.urlopen('https://en.wikipedia.org/wiki/Awards_and_decorations_of_the_United_States_Armed_Forces');
+
+soup = BeautifulSoup(wiki.read(), 'html.parser')
+
+image_tags = soup.table.find_all('img')
+
+titles = []
+images = []
+links = []
+for image_tag in image_tags:
+	schema = image_tag['src'].replace('/thumb','').split('/')
+	schema.pop()
+	content = image_tag.parent.findNext('a')
+	title = content.text
+	link = content['href']
+        image = 'https:'+ '/'.join(schema)
+	titles.append(title)
+	links.append(link)
+	#TODO: Download the actual image files into
+	#a directory off the root
+	images.append(image)
+	
+conn = sqlite3.connect('ribbons.sqlite')
+c = conn.cursor()
+
+is_same_size = len(titles) == len(images) == len(links)
+if is_same_size:
+	for x in range(0,len(images)):
+		c.execute('insert into ribbons(name,description,image) values(?,?,?)',[titles[x],links[x],images[x]])
+
+c.execute('select count(id) from ribbons')
+
+status_message = 'Download Successful!!'
+if len(titles) != c.fetchone()[0]:
+	status_message = 'Download Unsuccessful!!'
+print status_message        
+
+conn.commit()
+conn.close()
